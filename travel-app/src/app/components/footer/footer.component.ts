debb32190e41bc9e7e59740104fdf8681ff1ea0a
AresGn <aresgnimagnon0@gmail.com>
2025-05-17 17:09:21 +0100

    Enhance travel app with new features and styles. Added framer-motion for animations, improved layout and styling for components including header, footer, testimonials, and home page. Introduced new assets and updated Angular configuration for asset management.

--- travel-app/src/app/components/footer/footer.component.ts
+++ travel-app/src/app/components/footer/footer.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
